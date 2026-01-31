0be04dd296f6a09e90f4d8aad67263c804e395f4
pratiksha033 <pratikshaparihar679@gmail.com>
2026-01-31 14:02:54 +0530

    update socket.js for production

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
-  autoConnect: true,
-});
+const URL =
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+export const socket = io(URL);
